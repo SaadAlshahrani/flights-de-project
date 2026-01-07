1bd7232352277d45a1b171f536a23ec8f54c4dd1
SaadAlshahrani <110533228+SaadAlshahrani@users.noreply.github.com>
2026-01-07 19:54:08 +0300

    create first commit to project
    This is the first commit to the flights data engineering project.
    Created ingestion script that fetches from aviationstack API and stores
    the data it received in the 'data' folder.

---
+++ ingestion/fetch_flights.py
@@ -0,0 +1,73 @@
+import requests
+import os
+import json
+import logging
+
+from datetime import date
+from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
+
+logging.basicConfig(
+    level=logging.INFO,
+    filename='app.log',
+    encoding='utf-8',
+    filemode='a',
+    style='{',
+    format='{asctime} - {levelname} - {message}',
+    datefmt='%Y-%m-%d %H:%M',
+)
+
+API_KEY = os.getenv['FLIGHTS_API_KEY']
+BASE_URL = 'https://api.aviationstack.com/v1/flights'
+
+params = {
+    'access_key': API_KEY,
+    'dep_iata': 'JED',
+    'limit': 100
+}
+
+try:
+    logging.info(f"Requesting flights from {params['dep_iata']}")
+    response = requests.get(BASE_URL, params=params, timeout=10)
+    response.raise_for_status()
+    data = response.json()
+    logging.info(f"Records received: {len(data.get('data', []))}")
+
+    if "data" not in data:
+        raise ValueError("API response missing 'data' field.")
+    
+    if not data['data']:
+        logging.warning("API returned zero flights")
+
+except requests.exceptions.Timeout:
+    logging.error(f"Timeout error: The request to {BASE_URL} timed out.")
+    
+except requests.exceptions.ConnectionError:
+    logging.error(f"Connection error: Could not connect to the server at {BASE_URL}. Check network connectin or URL.")
+
+except requests.exceptions.HTTPError as e:
+    logging.error(f"HTTP error: An HTTP error occured: {e}")
+
+except requests.exceptions.RequestException as e:
+    logging.error(f"Error: An unexpected error occured during the request: {e}")
+
+except json.JSONDecodeError:
+    logging.error(f"JSON Error: Failed to decode JSON from request body.")
+
+except Exception as e:
+    logging.error(f"Error: An unexpected error occured: {e}")
+
+else:
+    logging.info("Successfully fetched data.")
+
+    today = date.today().isoformat()
+    output_dir = Path(f"data/raw/flights/{today}")
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    with open(output_dir / 'flights.json', 'w') as f:
+        json.dump(data, f, indent=2)
+
+finally:
+    logging.info(f"API request attempt finished.")
