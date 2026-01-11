73c5481afd19512fe221b26d36e169f89ca233ee
SaadAlshahrani <110533228+SaadAlshahrani@users.noreply.github.com>
2026-01-11 10:51:36 +0300

    refactor flights ingestion code

--- ingestion/fetch_flights.py
+++ ingestion/fetch_flights.py
@@ -1,13 +1,10 @@
 # TODO: use pydantic or python-decouple for env variables
-# TODO: refactor code to use main function
-# TODO: add ingestion time to data
 
 import requests
 import os
 import json
 import logging
-
-from datetime import date
+from datetime import date, datetime
 from pathlib import Path
 from dotenv import load_dotenv
 
@@ -15,63 +12,99 @@ load_dotenv()
 
 logging.basicConfig(
     level=logging.INFO,
-    filename='app.log',
-    encoding='utf-8',
-    filemode='a',
-    style='{',
-    format='{asctime} - {levelname} - {message}',
-    datefmt='%Y-%m-%d %H:%M',
+    filename="fetch_flights.log",
+    encoding="utf-8",
+    filemode="a",
+    style="{",
+    format="{asctime} - {levelname} - {message}",
+    datefmt="%Y-%m-%d %H:%M",
 )
 
-API_KEY = os.environ['FLIGHTS_API_KEY']
-BASE_URL = 'https://api.aviationstack.com/v1/flights'
+API_KEY = os.environ["FLIGHTS_API_KEY"]
+BASE_URL = "https://api.aviationstack.com/v1/flights"
 
-params = {
-    'access_key': API_KEY,
-    'dep_iata': 'JED',
-    'limit': 100
-}
+params = {"access_key": API_KEY, "dep_iata": "JED", "limit": 100}
 
-try:
-    logging.info(f"Requesting flights from {params['dep_iata']}")
-    response = requests.get(BASE_URL, params=params, timeout=10)
-    response.raise_for_status()
-    data = response.json()
-    logging.info(f"Records received: {len(data.get('data', []))}")
 
-    if "data" not in data:
-        raise ValueError("API response missing 'data' field.")
-    
-    if not data['data']:
-        logging.warning("API returned zero flights")
+def fetch_flight_data():
+    try:
+        logging.info(f"Requesting flights from {params['dep_iata']}")
+        response = requests.get(BASE_URL, params=params, timeout=10)
+        response.raise_for_status()
+        data = response.json()
+        logging.info(f"Records received: {len(data.get('data', []))}")
 
-except requests.exceptions.Timeout:
-    logging.error(f"Timeout error: The request to {BASE_URL} timed out.")
-    
-except requests.exceptions.ConnectionError:
-    logging.error(f"Connection error: Could not connect to the server at {BASE_URL}. Check network connectin or URL.")
+        if "data" not in data:
+            raise ValueError("API response missing 'data' field.")
 
-except requests.exceptions.HTTPError as e:
-    logging.error(f"HTTP error: An HTTP error occured: {e}")
+        if not data["data"]:
+            logging.warning("API returned zero flights")
 
-except requests.exceptions.RequestException as e:
-    logging.error(f"Error: An unexpected error occured during the request: {e}")
+    except requests.exceptions.Timeout:
+        logging.error(f"Timeout error: The request to {BASE_URL} timed out.")
 
-except json.JSONDecodeError:
-    logging.error(f"JSON Error: Failed to decode JSON from request body.")
+    except requests.exceptions.ConnectionError:
+        logging.error(
+            f"Connection error: Could not connect to the server at {BASE_URL}. Check network connectin or URL."
+        )
 
-except Exception as e:
-    logging.error(f"Error: An unexpected error occured: {e}")
+    except requests.exceptions.HTTPError as e:
+        logging.error(f"HTTP error: An HTTP error occured: {e}")
 
-else:
-    logging.info("Successfully fetched data.")
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Error: An unexpected error occured during the request: {e}")
 
+    except json.JSONDecodeError:
+        logging.error(f"JSON Error: Failed to decode JSON from request body.")
+
+    except Exception as e:
+        logging.error(f"Error: An unexpected error occured: {e}")
+
+    else:
+        logging.info("Successfully fetched data.")
+        return data
+        
+    finally:
+        logging.info(f"API request attempt finished.")
+
+
+def save_raw_flights(data):
     today = date.today().isoformat()
-    output_dir = Path(f"data/raw/flights/{today}")
-    output_dir.mkdir(parents=True, exist_ok=True)
+    timestamp = datetime.now()
+    payload = {
+        "metadata": {
+            "ingested_at": timestamp.isoformat(),
+        },
+        "data": data,
+    }
 
-    with open(output_dir / 'flights.json', 'w') as f:
-        json.dump(data, f, indent=2)
+    try:
+        output_dir = Path(f"data/raw/flights/{today}")
+        output_dir.mkdir(parents=True, exist_ok=True)
 
-finally:
-    logging.info(f"API request attempt finished.")
+    except (TypeError, PermissionError, OSError) as e:
+        logging.error(f"Failed to create output directory. {e}")
+
+    try:
+        with open(
+            output_dir / f'{timestamp.strftime("%Y%m%d%H%M%S")}_flights.json', "w"
+        ) as f:
+            json.dump(payload, f, indent=2)
+
+    except (PermissionError, OSError) as e:
+        logging.error(f"Failed to create output file. {e}")
+
+    except TypeError as e:
+        logging.error(f"Payload is not JSON serializable. {e}")
+
+    except KeyboardInterrupt as e:
+        logging.error(f"Operation has been interrupted. {e}")
+
+
+def main():
+    data = fetch_flight_data()
+    save_raw_flights(data)
+
+
+if __name__ == "__main__":
+    main()
